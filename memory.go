3473f2acd6b2f5784e87eadddf8c612630c09510
dillon <dillonstreator@gmail.com>
2022-05-12 00:38:41 -0500

    export comments

--- memory.go
+++ memory.go
@@ -6,6 +6,7 @@ import (
 	"time"
 )
 
+//MemoryThrottler in memory throttler
 type MemoryThrottler struct {
 	mus     map[string]*sync.Mutex
 	history map[string]time.Time
@@ -15,6 +16,7 @@ type MemoryThrottler struct {
 
 var _ Throttler = (*MemoryThrottler)(nil)
 
+//NewMemoryThrottler creates a new MemoryThrottler
 func NewMemoryThrottler() *MemoryThrottler {
 	return &MemoryThrottler{
 		mus:     map[string]*sync.Mutex{},
@@ -22,14 +24,17 @@ func NewMemoryThrottler() *MemoryThrottler {
 	}
 }
 
+//DefaultMemoryThrottler a default MemoryThrottler
 var DefaultMemoryThrottler = NewMemoryThrottler()
 
+//New curried in memory throttling
 func (m *MemoryThrottler) New(key string, duration time.Duration, fn func(ctx context.Context) error) func(context.Context) error {
 	return func(ctx context.Context) error {
 		return m.do(ctx, key, duration, fn)
 	}
 }
 
+//Do inline in memory throttling
 func (m *MemoryThrottler) Do(ctx context.Context, key string, duration time.Duration, fn func(ctx context.Context) error) error {
 	return m.do(ctx, key, duration, fn)
 }
--- redis.go
+++ redis.go
@@ -9,24 +9,28 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
+//RedisThrottler redis throttler
 type RedisThrottler struct {
 	redisClient redis.UniversalClient
 }
 
 var _ Throttler = (*RedisThrottler)(nil)
 
+//NewRedisThrottler creates a new RedisThrottler
 func NewRedisThrottler(redisClient redis.UniversalClient) *RedisThrottler {
 	return &RedisThrottler{
 		redisClient: redisClient,
 	}
 }
 
+//New curried redis throttling
 func (r *RedisThrottler) New(key string, duration time.Duration, fn func(ctx context.Context) error) func(context.Context) error {
 	return func(ctx context.Context) error {
 		return r.do(ctx, key, duration, fn)
 	}
 }
 
+//New inline redis throttling
 func (r *RedisThrottler) Do(ctx context.Context, key string, duration time.Duration, fn func(ctx context.Context) error) error {
 	return r.do(ctx, key, duration, fn)
 }
